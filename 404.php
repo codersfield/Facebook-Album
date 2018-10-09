b10615205557104dca9d7ffc47014e29ae2287e8
Matthew Magin <matthew.d.magin@gmail.com>
2018-10-10 07:01:51 +1000

    Updated 404 page to make it look pretty with particle.js
    Deleted old HTML 404 page. Used a php file and implemented particle.js

---
+++ 404.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>Particles background using Particles.js</title>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+  
+      <link rel="stylesheet" href="css/style.css">
+
+  
+</head>
+
+<body>
+
+  <div id="particles-js"></div>
+
+<div class="text">
+	<h1>404 Error</h1>
+	<p>How About Going <a href="index.php">Home</a> Instead ¯\_(ツ)_/¯</p>
+</div>
+  <script src='https://cldup.com/S6Ptkwu_qA.js'></script>
+
+  
+
+    <script  src="js/particle.js"></script>
+
+
+
+
+</body>
+
+</html>
