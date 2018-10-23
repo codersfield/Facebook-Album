0fccfe6bda20dce32d4900240f22644395b7f22e
Andrea Martini <andrea.martini@travelappeal.com>
2018-10-23 12:32:15 +0200

    updated with composer

--- config.php
+++ config.php
@@ -4,8 +4,8 @@
 	require_once "Facebook/autoload.php";
 
 	$FB = new \Facebook\Facebook([
-		'app_id' => 'Your_App_ID',
-		'app_secret' => 'Your_Secret_Key',
+		'app_id' => '806834059419905',
+		'app_secret' => '88863b2c7c655405a574193d75350e75',
 		'default_graph_version' => 'v3.1'
 	]);
 
